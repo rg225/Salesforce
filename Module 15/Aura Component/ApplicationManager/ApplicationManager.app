50d9c9fd439a79f6bbda3ca6d87d56b7145fb1d1
gopal gupta <gopal.gupta@metacube.com>
2020-05-18 14:27:08 +0530

    Review Changes

--- Module 15/Aura Component/ApplicationManager/ApplicationManager.app
+++
@@ -1,3 +0,0 @@
-<aura:application extends="force:slds">
-    <c:ContactCreate/>
-</aura:application>
--- Module 15/Aura Component/ContactCreate/ContactCreateController.js
+++ Module 15/Aura Component/ContactCreate/ContactCreateController.js
@@ -1,33 +1,18 @@
 ({
-    doSave: function(component, event, helper) {
-        var action = component.get("c.createContact");
-
-        // To validate inputs
-        var validContact = component.find('contactform').reduce(function(validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-
-        if (validContact) {
-            // Create the new contact
-            var contactList = component.get("v.contactList");
-            action.setParams({ 'contObj': component.get('v.contactObj') });
-            action.setCallback(this, function(data) {
-                component.set('v.contactName', data.getReturnValue())
-                contactList.splice(0, 0, component.get('v.contactObj'));
-                component.set("v.contactList", contactList);
-            });
-            $A.enqueueAction(action);
-        }
+    handleSuccess: function(component, event, helper) {
+        component.find('notifLib').showToast({
+            "variant": "success",
+            "title": "Contact Created",
+            "message": "Record ID: " + event.getParam("id")
+        });
+        window.open('../r/Contact/' + event.getParam("id") + '/view', '_top')
+    },
+    handleError: function(component, event, helper) {
+        component.find('notifLib').showToast({
+            "variant": "error",
+            "title": "Error",
+            "message": "Something has gone wrong!"
+        });
     },
 
-    // To fetch contact list and add into Contact array
-    showContacts: function(component, event, helper) {
-        var action = component.get("c.retrieveContacts");
-        action.setCallback(this, function(data) {
-            component.set('v.contactList', data.getReturnValue())
-        });
-        $A.enqueueAction(action);
-    }
 })
