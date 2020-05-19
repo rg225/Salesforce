3634a723550cda507f81456d2c8ed63b6be938b3
meta-gopal-gupta <59638331+meta-gopal-gupta@users.noreply.github.com>
2020-05-19 15:46:44 +0530

    Review changes for redirect code

--- Module 15/Web Component/createContact/createContact.js
+++ Module 15/Web Component/createContact/createContact.js
@@ -1,18 +1,29 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import CONTACT_SCHEMA from './constants';
 
-export default class ContactComponent extends LightningElement {
+export default class ContactComponent extends NavigationMixin(LightningElement) {
     contactRecord = CONTACT_SCHEMA;
 
     handleSuccess(event) {
         const evt = new ShowToastEvent({
-            title: "Contact created",
+            title: "Contact Created",
             message: "Record ID: " + event.detail.id,
             variant: "success"
         });
         this.dispatchEvent(evt);
-        window.open('../r/Contact/' + event.detail.id + '/view', '_top');
+
+        // To redirect on the contact record page
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: event.detail.id,
+                objectApiName: 'Contact',
+                actionName: 'view',
+            },
+        });
+
     }
     handleError(event) {
         const evt = new ShowToastEvent({
